d22603105c3c9f7a8cee78194fb4e8157b21bded
liangjianlin <nbaliangjianlin@qq.com>
2016-08-27 09:56:48 +0800

    20160827

---
+++ javascript.js
@@ -0,0 +1,22 @@
+window.onload = function (argument) {
+	for (var i = 6; i >= 1; i--) {
+		list_menu('list' + i, 'list-item-' + i);
+	}
+}
+
+/**
+* 文章目录的子目录的显示和隐藏
+* list 文章目录
+* list_item 文章目录的子目录
+*/
+function list_menu(list, list_item) {
+	var l = document.getElementById(list);
+	l.onclick = function() {
+		var l_item = document.getElementById(list_item);
+		if (l_item.style.display == 'none') {
+			l_item.style.display = 'block';
+		} else {
+			l_item.style.display = 'none';
+		}
+	};
+}
